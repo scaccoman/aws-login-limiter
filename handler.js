6e22390394663957b805364ffb3a0acc64a4ea90
scaccoman <scaccoman@gmail.com>
2020-05-05 13:50:33 +0100

    add handler imports

---
+++ handler.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports.authenticate = require('./handlers/authenticate');
+module.exports.logout = require('./handlers/logout');
+module.exports.login = require('./handlers/login');
